11467df1d9cc04477788a5ddf7c3522e60ad051f
FatmaBuseBorlu <fatmabuseborlu@gmail.com>
2024-10-08 20:46:02 +0300

    Add project files.

---
+++ Project2WooxTravel/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2WooxTravel.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+      
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
