a889f960b366bfa52f3d8a3a6f4da43be66b52aa
FatmaBuseBorlu <fatmabuseborlu@gmail.com>
2024-10-15 21:03:24 +0300

    features

--- Project2WooxTravel/Areas/Admin/Controllers/DestinationController.cs
+++ Project2WooxTravel/Areas/Admin/Controllers/DestinationController.cs
@@ -16,5 +16,47 @@ namespace Project2WooxTravel.Areas.Admin.Controllers
             var values = context.Destinations.ToList();
             return View(values);
         }
+        public ActionResult CreateDestination()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult CreateDestination(Destination destination)
+        {
+            context.Destinations.Add(destination);
+            context.SaveChanges();
+            return RedirectToAction("DestinationList", "Destination", "Admin");
+        }
+        public ActionResult DeleteDestination(int id)
+        {
+            var value = context.Destinations.Find(id);
+            context.Destinations.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("DestinationList", "Destination", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateDestination(int id)
+        {
+            var value = context.Destinations.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateDestination(Destination destination)
+        {
+            var value = context.Destinations.Find(destination.DestinationId);
+            value.Description = destination.Description;
+            value.City = destination.City;
+            value.DayNight = destination.DayNight;
+            value.Country = destination.Country;
+            value.ImageUrl = destination.ImageUrl;
+            value.Price = destination.Price;
+            value.Title = destination.Title;
+            context.SaveChanges();
+            return RedirectToAction("DestinationList", "Destination", "Admin");
+        }
+
     }
 }
